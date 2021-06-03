b2a7e0f1a524fbc53c77eb9834629cce76883368
Batrak Yurii <navid7498@gmail.com>
2021-06-03 14:58:07 +0300

    Stack

--- Program.cs
+++ Program.cs
@@ -14,17 +14,7 @@ namespace HomeTask
         /// <param name="args">arrayStack - an object of type Stack.</param>
         public static void Main(string[] args)
         {
-            var arrayStack = new Stack<string>();
-            arrayStack.Push("First element");
-            arrayStack.Push("Second element");
-            arrayStack.Push("Third element");
-
-            Console.WriteLine(arrayStack.Peek());
-
-            arrayStack.Pop();
-            arrayStack.Push("Last element");
-
-            Console.WriteLine(arrayStack.Peek());
+            Console.WriteLine("Hello");
         }
     }
 }
