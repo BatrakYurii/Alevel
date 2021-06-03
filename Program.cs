c48ecfd65a39376fae186d0c99a565ee14dd5ccd
Batrak Yurii <navid7498@gmail.com>
2021-06-03 14:38:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeTask
+{
+    /// <summary>
+    /// Main Program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Class Program.
+        /// </summary>
+        /// <param name="args">arrayStack - an object of type Stack.</param>
+        public static void Main(string[] args)
+        {
+            var arrayStack = new Stack<string>();
+            arrayStack.Push("First element");
+            arrayStack.Push("Second element");
+            arrayStack.Push("Third element");
+
+            Console.WriteLine(arrayStack.Peek());
+
+            arrayStack.Pop();
+            arrayStack.Push("Last element");
+
+            Console.WriteLine(arrayStack.Peek());
+        }
+    }
+}
